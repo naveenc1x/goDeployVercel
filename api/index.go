67882fffca5affb060fc98e438cf779c685261fb
naveenc1x <naveenkumar.viswanathan@c1exchange.com>
2023-11-07 23:35:27 +0530

    chagned to fiber

--- api/index.go
+++ api/index.go
@@ -1,46 +1,20 @@
-
-package handler
+package main
 
 import (
+	"log"
 	"fmt"
-	"net/http"
 
-	. "github.com/tbxark/g4vercel"
+	"github.com/gofiber/fiber/v2"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	server := New()
+func main() {
+	app := fiber.New()
 
-	server.GET("/", func(context *Context) {
-		context.JSON(200, H{
-			"message": "hello go from vercel !!!!",
-		})
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
 	})
-	server.GET("/hello", func(context *Context) {
-		name := context.Query("name")
-		if name == "" {
-			context.JSON(400, H{
-				"message": "name not found",
-			})
-		} else {
-			context.JSON(200, H{
-				"data": fmt.Sprintf("Hello %s!", name),
-			})
-		}
-	})
-	server.GET("/user/:id", func(context *Context) {
-		context.JSON(400, H{
-			"data": H{
-				"id": context.Param("id"),
-			},
-		})
-	})
-	server.GET("/long/long/long/path/*test", func(context *Context) {
-		context.JSON(200, H{
-			"data": H{
-				"url": context.Path,
-			},
-		})
-	})
-	server.Handle(w, r)
+
+	fmt.Println("Listening on port 3000")
+
+	log.Fatal(app.Listen(":3000"))
 }
